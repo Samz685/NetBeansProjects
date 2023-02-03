8c7a1eee0fed30d32bbe59ca580009acdfcab3a6
Samz685 <michan.s254812@cesurformacion.com>
2023-02-03 15:05:08 +0100

    eliminando imports innecesarios

--- ComandasFxFib/ComandasFxFib/src/main/java/com/mycompany/loginfxml/Informe.java
+++ ComandasFxFib/ComandasFxFib/src/main/java/com/mycompany/loginfxml/Informe.java
@@ -1,21 +1,13 @@
 
 package com.mycompany.loginfxml;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
 import javax.swing.JFrame;
 import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.engine.export.JRPdfExporter;
-import net.sf.jasperreports.export.SimpleExporterInput;
-import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
-import net.sf.jasperreports.export.SimplePdfExporterConfiguration;
 import net.sf.jasperreports.swing.JRViewer;
-import org.hibernate.Session;
 
 
 public class Informe {
